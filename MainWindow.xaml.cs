c921060ef55b3d55600470a34b830c8bb30578c3
Ali Iktider <asayam@contract.planetdds.com>
2023-06-15 01:25:27 -0400

    merged with own logic for login - mainwindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 using BookStoreLIB;
 
 namespace BookStoreGUI
@@ -28,13 +29,40 @@ namespace BookStoreGUI
             var dlg = new LoginDialog();
             dlg.Owner = this;
             dlg.ShowDialog();
-            // Process data entered by user if dialog box is accepted
-            if (dlg.DialogResult == true)
+
+            string username = dlg.nameTextBox.Text;
+            string pwd = dlg.passwordTextBox.Password;
+            Match match = null;
+
+            Regex r = new Regex("^[a-zA-Z][0-9a-zA-Z]{5,}");
+
+            MatchCollection regexCheck = r.Matches(pwd);
+            if (r.Matches(pwd).Count > 0)
             {
-                if (userData.LogIn(dlg.nameTextBox.Text, dlg.passwordTextBox.Password) == true)
-                    this.statusTextBlock.Text = "You are logged in as User #" + userData.UserID;
+                match = regexCheck[0];
+            }
+
+
+            if (username == "" || pwd == "")
+            {
+                MessageBox.Show("Please fill in all the slots.");
+            }
+            else
+            {
+                if (match != null && match.Length == pwd.Length)
+                {
+                    if (userData.LogIn(username, pwd) == true)
+                    {
+                        this.statusTextBlock.Text = "You are logged in as User #" + userData.UserID;
+                        MessageBox.Show("You are logged in as User # " + userData.UserID);
+                    }
+                    else
+                        MessageBox.Show("You could not be verified. Please try again.");
+                }
                 else
-                    MessageBox.Show("You could not be verified. Please try again.");
+                {
+                    MessageBox.Show("A valid password needs to have at least six characters starting with a letter containing both letters and numbers.");
+                }
             }
         }
         private void exitButton_Click(object sender, RoutedEventArgs e) { this.Close(); }
